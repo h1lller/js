1a3d6ded71ad73f5f0386063fa934703c7b23a75
Ivan <i.sprut@astoundcommerce.com>
2021-03-23 20:18:30 +0200

    Infinite images scroll

--- script.js
+++ script.js
@@ -1,49 +1,76 @@
 import 'regenerator-runtime/runtime'
-// dom elements
-const domQuote = document.querySelector('.js-q');
-const domAuthor = document.querySelector('.js-au');
-const domNew = document.querySelector('.js-new');
-const domTw = document.querySelector('.js-tw');
-const domCo = document.querySelector('#js-co');
 
-let apiQueries = []
-let Url = ''
-let loading = true
+const API_KEY = 'gbWOcrpqjX0v4Jravw_iWLsGcVaeu2sIkfP_MRJrGNM';
+const imagesContainer = document.querySelector('.js-images')
+const loader = document.querySelector('.js-loader')
 
-function fillQuoteText({text, author}) {
-    domQuote.textContent = text
-    domAuthor.textContent = author ? author : `Invisible rabbit`;
-    loading = false;
-    setTimeout(toggleLoader, Math.random() * Math.floor(apiQueries.length));
+let initialImagesCount = 2;
+let initialPageLoad = true;
+let fetchUrl = `https://api.unsplash.com/photos/random?count=${initialImagesCount}&client_id=${API_KEY}`;
+
+let fetchedPhotos = [];
+let readyToLoadMore = false;
+let imagesLoadedCount = 0;
+let totalImagesCount = 0;
+
+// helper function for adding initial attributes to images
+function setImageAttributes(element, obj) {
+    for (const key in obj) {
+        element.setAttribute(key, obj[key])
+    }
 }
 
-function getRandomQuoteFromQueries() {
-    const quote = apiQueries[Math.floor(Math.random() * Math.floor(apiQueries.length))];
-    Url = `https://twitter.com/intent/tweet?text=${quote.text} - ${quote.author}`;
-    loading = true;
-    toggleLoader();
-    fillQuoteText(quote);
+function imagesLoading() {
+    imagesLoadedCount++;
+    if (imagesLoadedCount === totalImagesCount) {
+        setTimeout(()=>{
+            loader.hidden = true;
+        }, 1000)
+        readyToLoadMore = true;
+        // check if page is loaded
+        if (initialPageLoad) {
+            initialPageLoad = false
+            initialImagesCount = 10;
+            fetchUrl = `https://api.unsplash.com/photos/random?count=${initialImagesCount}&client_id=${API_KEY}`;
+        }
+    }
 }
 
-function sendToTwitter() {
-    window.open(Url, '_blank');
+function displayPhotos() {
+    imagesLoadedCount = 0;
+    totalImagesCount = fetchedPhotos.length;
+    fetchedPhotos.forEach(photo => {
+        const image = document.createElement('img');
+        setImageAttributes(image, {
+            'alt': photo.alt_description,
+            'src': photo.urls.regular
+        })
+        imagesContainer.appendChild(image)
+        image.addEventListener('load', ()=>{
+            imagesLoading();
+        })
+    })
+
 }
 
-async function getQueries() {
-    const url = 'https://type.fit/api/quotes';
+async function getPhotos() {
     try {
-        const response = await fetch(url);
-        apiQueries = await response.json();
-        getRandomQuoteFromQueries(apiQueries);
+        const response = await fetch(fetchUrl);
+        fetchedPhotos = await response.json();
+        displayPhotos();
     } catch (e) {
         console.log(e)
     }
 }
 
-function toggleLoader() {
-    loading ? domCo.classList.add('is-loading') : domCo.classList.remove('is-loading');
+function checkScrolledArea() {
+    let scrollArea = window.innerHeight + window.scrollY;
+    if(scrollArea > (document.body.offsetHeight - 500) && readyToLoadMore) {
+        readyToLoadMore = false;
+        getPhotos();
+    }
 }
 
-getQueries();
-domNew.addEventListener('click', getRandomQuoteFromQueries)
-domTw.addEventListener('click', sendToTwitter)
+window.addEventListener('scroll', checkScrolledArea)
+
+getPhotos();
